b6f73cee1eb624624ef03558d032c35125d57d6a
Valerie Boem <vboem@gmail.com>
2017-02-06 17:20:01 -0500

    Creating log in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ root 'products#index'
 
   resources :products
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

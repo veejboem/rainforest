e1c835603ffe6430b2309303df9c705eba5c4ad3
Valerie Boem <vboem@gmail.com>
2017-02-06 15:05:33 -0500

    Generating the products controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get 'products/:id/edit' => "products#edit", as: "edit_product"
+
+  resources :products
 end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
